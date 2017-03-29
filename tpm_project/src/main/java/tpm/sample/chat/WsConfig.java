e41697b94e7c0bbf68c5acd9a0f12f2511df83c2
user1 <user1@KH_H>
2017-03-29 14:36:24 +0900

    dddd

---
+++ tpm_project/src/main/java/tpm/sample/chat/WsConfig.java
@@ -0,0 +1,32 @@
+package tpm.sample.chat;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WsConfig implements WebSocketConfigurer {
+
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(echoHandler(), "/echo-ws");
+		registry.addHandler(chatHandler(), "/chat-ws");
+		registry.addHandler(echoHandler(), "/echo.sockjs").withSockJS();
+		registry.addHandler(chatHandler(), "/chat.sockjs").withSockJS();
+	}
+
+	@Bean
+	public EchoHandler echoHandler() {
+		return new EchoHandler();
+	}
+	
+	@Bean
+	public ChatWebSocketHandler chatHandler() {
+		return new ChatWebSocketHandler();
+	}
+
+}
