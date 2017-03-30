60743bab9198b0de97683aafc36368b485879262
user1 <user1@KH_H>
2017-03-30 13:52:21 +0900

    asd

--- tpm_project/src/main/java/tpm/checklist/model/ChecklistDAO.java
+++ tpm_project/src/main/java/tpm/checklist/model/ChecklistDAO.java
@@ -2,4 +2,7 @@ package tpm.checklist.model;
 
 public interface ChecklistDAO {
 
+	public int addChecklist();
+	public int updateChecklist();
+	public int delChecklist();
 }
---
+++ tpm_project/src/main/java/tpm/member/model/MemberDAO.java
@@ -0,0 +1,5 @@
+package tpm.member.model;
+
+public class MemberDAO {
+
+}
