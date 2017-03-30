48f95088bf10b71a3637098eaa7f7dbf674315d0
user1 <user1@KH_H>
2017-03-30 14:22:12 +0900

    파일 DAO

--- tpm_project/src/main/java/tpm/file/model/FileDAO.java
+++ tpm_project/src/main/java/tpm/file/model/FileDAO.java
@@ -6,9 +6,9 @@ import tpm.project.model.ProjectDTO;
 
 public interface FileDAO {
 	
-	public ArrayList<FileDTO>projectAllList();
-	public getFileList();
-	public getFileContent();
+	public ArrayList<FileDTO> projectAllList();
+	public ArrayList<FileDTO> getFileList();
+	public int searchFile();
 	public int addFile();
 	public int updateFile();
 	public int delFile();
