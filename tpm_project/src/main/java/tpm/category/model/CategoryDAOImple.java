b703876cd58ec7e8ba687956323fa2ea12763f68
user1 <user1@KH_H>
2017-04-03 10:45:43 +0900

    test

---
+++ tpm_project/src/main/java/tpm/category/model/CategoryDAOImple.java
@@ -0,0 +1,20 @@
+package tpm.category.model;
+
+public class CategoryDAOImple implements CategoryDAO {
+
+	public int categoryInsert(CategoryDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int categoryUpdate(CategoryDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int categoryDelete(int category_idx) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- tpm_project/src/main/java/tpm/controller/CategoryController.java
+++ tpm_project/src/main/java/tpm/controller/CategoryController.java
@@ -1,17 +1,23 @@
 package tpm.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import tpm.category.model.CategoryDAO;
+
 @Controller
 public class CategoryController {
-
+	
+	@Autowired
+	CategoryDAO dao;
+	
 	//// 카테고리 ////
 	/** 카테고리 - 카테고리 추가 */
 	@RequestMapping(value="categoryAdd.do",  method=RequestMethod.POST)
-	public ModelAndView categoryAdd(){
+	public ModelAndView categoryAdd(String category_name){
 		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("category/categoryResult_d");
