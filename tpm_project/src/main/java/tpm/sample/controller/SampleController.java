c2ee2d412a62f4b0f361bd9fa5f5d862444a930f
user1 <user1@KH_H>
2017-04-06 12:38:01 +0900

    dd

--- tpm_project/src/main/java/tpm/sample/controller/SampleController.java
+++ tpm_project/src/main/java/tpm/sample/controller/SampleController.java
@@ -3,6 +3,7 @@ package tpm.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SampleController {
@@ -11,5 +12,15 @@ public class SampleController {
 	@RequestMapping(value="popover.do", method=RequestMethod.GET)
 	public String popover(){
 		return "popover";
+	}
+	
+	/** DTO 안 DTO 테스트 */
+	@RequestMapping(value="dtotest.do", method=RequestMethod.GET)
+	public ModelAndView dtoTest(){
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("dto");
+		
+		return mav;
 	}
 }
