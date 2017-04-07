69820557f3db43bdb2e496eb3b94f60a1972b4fb
user1 <user1@KH_H>
2017-04-07 12:47:49 +0900

    ㄴㅇㄹㅇ

--- tpm_project/src/main/java/tpm/sample/controller/SampleController.java
+++ tpm_project/src/main/java/tpm/sample/controller/SampleController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import tpm.member.model.MemberDAO;
 import tpm.member.model.MemberDTO;
 import tpm.sample.model.SampleWorkDAO;
@@ -65,5 +68,56 @@ public class SampleController {
 		mav.setViewName("sample/json/jsonResult1");
 		return mav;
 	}
+	
+	/** JSON-SIMPLE 사용하여 결과 페이지에서 JSON 데이터를 만들기 */
+	@RequestMapping(value="jsontest2.do", method=RequestMethod.GET)
+	public ModelAndView jsonTest2(){
+		ModelAndView mav = new ModelAndView();
+		
+		ArrayList<MemberDTO> arry_mdto = mdao.getMemberAllList();
+		mav.addObject("arry_mdto", arry_mdto);
+		
+		mav.setViewName("sample/json/jsonResult2");
+		return mav;
+	}
+	
+	/** net.sf.json-lib 의 JSONArray, JSONObject의 fromObject 를 이용하여 리스트를 JSON 데이터로 변환후 결과 페이지로 보내기 */
+	@RequestMapping(value="jsontest3.do", method=RequestMethod.GET)
+	public ModelAndView jsonTest3(){
+		ModelAndView mav = new ModelAndView();
+		
+		ArrayList<MemberDTO> arry_mdto = mdao.getMemberAllList();
+
+		JSONArray jsonArray = new JSONArray();
+		JSONObject json = new JSONObject();
+
+		json.put("members", jsonArray.fromObject(arry_mdto));
+		
+		mav.setViewName("sample/json/jsonResult3");
+		mav.addObject("json", json);
+		return mav;
+	}
+	
+	/** SPRING 어노테이션을 이용하여 컨트롤러에서 LIST를 자동으로 JSON으로 변환하여 결과 페이지로 보내기 */
+	@RequestMapping(value="jsontest4.do", method=RequestMethod.GET)
+	public @ResponseBody ArrayList<MemberDTO> jsonTest4(){
+
+		ArrayList<MemberDTO> arry_mdto = mdao.getMemberAllList();
+
+		return arry_mdto;
+	}
+	
+	/** jsonView를 이용하여 자동으로 임의의 결과 페이지로 List를 json 데이터 변환하여 보내기 */
+	@RequestMapping(value="jsontest5.do", method=RequestMethod.GET)
+	public ModelAndView jsonTest5(){
+
+		ArrayList<MemberDTO> arry_mdto = mdao.getMemberAllList();
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("members_test", arry_mdto);
+		mav.addObject("members_test2", arry_mdto);
+		mav.setViewName("jsonView");
+		return mav;
+	}
+	
 	
 }
--- tpm_project/src/main/webapp/WEB-INF/view/sample/json/jsonResult1.jsp
+++ tpm_project/src/main/webapp/WEB-INF/view/sample/json/jsonResult1.jsp
@@ -1,11 +1,45 @@
+<%@page import="tpm.member.model.MemberDTO"%>
+<%@page import="java.util.ArrayList"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!-- 결과 페이지에서 수작업을 통하여 JSON 형식의 데이터를 구축하기 -->
+<!-- 방법 1. -->
+<%
+	ArrayList<MemberDTO> arry_mdto2 = (ArrayList) request.getAttribute("arry_mdto");
+%>
 
-</body>
-</html>
+{
+	"members" : [
+<%
+		for(int i = 0 ; i < arry_mdto2.size() ; i++){
+%>
+			{"member_idx":<%=arry_mdto2.get(i).getMember_idx()%>,"member_id":"<%=arry_mdto2.get(i).getMember_id()%>","member_name":"<%=arry_mdto2.get(i).getMember_name()%>"}
+<%
+			if(i != arry_mdto2.size()-1){
+%>
+			,
+<%				
+			}
+		}
+%>
+	]
+}
+<hr>
+<hr>
+<!-- 방법 2. -->
+{
+	"members" : [
+		<c:if test="${!empty arry_mdto}">
+			<c:forEach var="i" begin="0" end="${arry_mdto.size() - 1}" step="1">
+				{"member_idx":${arry_mdto.get(i).member_idx},"member_id":"${arry_mdto.get(i).member_id}","member_name":"${arry_mdto.get(i).member_name}"}
+				<c:if test="${i != arry_mdto.size() - 1}">
+				,
+				</c:if>
+			</c:forEach>
+		</c:if>
+	]
+	
+}
+
+
+
