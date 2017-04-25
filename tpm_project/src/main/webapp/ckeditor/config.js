80096be95cb7dd3012ac693c4645bb5eb615f302
user1 <user1@KH_H>
2017-04-25 15:20:24 +0900

    dfdf

--- tpm_project/src/main/webapp/ckeditor/config.js
+++ tpm_project/src/main/webapp/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// %REMOVE_START%
 	// The configuration options below are needed when running CKEditor from source files.
 	config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,clipboard,button,panelbutton,panel,floatpanel,colorbutton,templates,menu,contextmenu,copyformatting,div,resize,toolbar,elementspath,enterkey,popup,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,undo,wsc,lineutils,widgetselection,widget,gg,youtube,filetools,notification,notificationaggregator,uploadwidget,uploadimage,uploadfile,videoembed,placeholder';
-	config.skin = 'office2013';
+	config.skin = 'Moono-Lisa';
 	//config.imageUploadUrl = '/tpm_project/uploader/upload.jsp';
 	config.filebrowserImageUploadUrl = 'chatImageUpload.do';
 	
